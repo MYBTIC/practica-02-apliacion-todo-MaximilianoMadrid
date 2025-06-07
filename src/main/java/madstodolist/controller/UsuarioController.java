2bcc975cba4796bcd2ef2c677da3f9aac2fc7d8a
MYBTIC <maxipillo2@gmail.com>
2025-06-07 13:21:46 -0500

    Agregado Controlador para Usuario
    Se agrego un GetMapping para /registrados

---
+++ src/main/java/madstodolist/controller/UsuarioController.java
@@ -0,0 +1,54 @@
+package madstodolist.controller;
+
+import madstodolist.authentication.ManagerUserSession;
+import madstodolist.controller.exception.UsuarioNoLogeadoException;
+import madstodolist.dto.UsuarioData;
+import madstodolist.service.UsuarioService;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @Autowired
+    private ManagerUserSession managerUserSession;
+
+    private void comprobarUsuarioLogeado(Long idUsuario) {
+        Long idUsuarioLogeado = managerUserSession.usuarioLogeado();
+        if (!idUsuario.equals(idUsuarioLogeado))
+            throw new UsuarioNoLogeadoException();
+    }
+
+    @GetMapping("/usuarios/{id}/cuenta")
+    public String formEditaCuenta(@PathVariable(value = "id") Long idUsuario, Model model) {
+        comprobarUsuarioLogeado(idUsuario);
+
+        UsuarioData usuario = usuarioService.findById(idUsuario);
+        model.addAttribute("usuario", usuario);
+        return "formCuentaUsuario";
+    }
+
+    @GetMapping("/registrados")
+    public String listadoUsuarios(Model model) {
+        List<UsuarioData> usuarios = usuarioService.findAll();
+        model.addAttribute("usuarios", usuarios);
+
+        // Añadimos también el usuario logeado para la navbar
+        Long idUsuarioLogeado = managerUserSession.usuarioLogeado();
+        if (idUsuarioLogeado != null) {
+            UsuarioData usuario = usuarioService.findById(idUsuarioLogeado);
+            model.addAttribute("usuario", usuario);
+        }
+
+        return "listaUsuarios";
+    }
+}
